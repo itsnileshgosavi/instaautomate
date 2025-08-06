7d0101468bde3923b892db2003ea9c0388dec026
Nilesh Gosavi <nbgosavi5@gmail.com>
2025-08-06 10:22:09 +0530

    added webhook

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    console.log(body);
+    return NextResponse.json({ message: "success" });
+}
+
+export async function GET(req: NextRequest) {
+    const VERIFY_TOKEN = "secret";
+    const mode = req.nextUrl.searchParams.get('hub.mode');
+    const token = req.nextUrl.searchParams.get('hub.verify_token');
+    const challenge = req.nextUrl.searchParams.get('hub.challenge');
+  
+    if (mode && token && mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('Webhook verified');
+      return NextResponse.json({ message: "success" });
+    }
+    return NextResponse.json({ message: "failure" });
+}
