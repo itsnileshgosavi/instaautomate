357e6d065c515e1b13c586e6c2c3cf4f8ce80e2c
Nilesh Gosavi <nbgosavi5@gmail.com>
2025-08-06 15:51:52 +0530

    f

--- src/services/getInstaAccount.ts
+++ src/services/getInstaAccount.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getInstagramBusinessAccount = async (token: string) => {
-    const response = await axios.get(`https://graph.instagram.com/v23.0/me?fields=id,username&access_token=${token}`);
-    const instaUserId = response.data.id;
-    return {instaUserId, username: response.data.username}
+    const response = await axios.get(`https://graph.instagram.com/v23.0/me?fields=id,username,user_id,name&access_token=${token}`);
+    const instaUserId = response.data.user_id;
+    return {instaUserId, username: response.data.username, name: response.data.name}
 }
