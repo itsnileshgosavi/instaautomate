64b8a2ed6689194298bd4a8c01793355cd2544e2
Nilesh Gosavi <nbgosavi5@gmail.com>
2025-08-06 11:06:43 +0530

    fix issue

--- src/services/getLongLIvedToken.ts
+++ src/services/getLongLIvedToken.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getLongLIvedToken = async (token: string) => {
-    const response = await axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&access_token=" + token`);
+    const response = await axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.INSTAGRAM_CLIENT_SECRET}&access_token=${token}`);
     const accessToken = response.data.access_token;
     return accessToken;
 }
