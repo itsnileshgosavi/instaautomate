838e3207cbdf84868f347263a954d5c7f37fee76
Nilesh Gosavi <nbgosavi5@gmail.com>
2026-03-27 15:11:31 +0530

    updating access token

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -4,80 +4,95 @@ import { getLongLIvedToken } from "@/services/getLongLIvedToken";
 import { AuthOptions } from "next-auth";
 import { getInstagramBusinessAccount } from "@/services/getInstaAccount";
 const authOptions: AuthOptions = {
-    providers: [
-      InstagramProvider({
-        clientId: process.env.INSTAGRAM_CLIENT_ID!,
-        clientSecret: process.env.INSTAGRAM_CLIENT_SECRET!,
-        authorization: {
-          params: {
-            scope: "instagram_business_basic",
-          },
+  providers: [
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID!,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "instagram_business_basic",
         },
-      }),
-    ],
-    session: {
-      strategy: "jwt",
-    },
-    callbacks: {
-      async signIn({ user, account }) {
-        if (account?.provider === "instagram") {
-          //search user in db by instagram id
-          const dbUser = await prisma.user.findUnique({
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "instagram") {
+        //search user in db by instagram id
+        const dbUser = await prisma.user.findUnique({
+          where: {
+            instagramId: user.id,
+          },
+        });
+        // update the access token with each new login
+        if (dbUser && account.access_token) {
+          const longLivedToken = await getLongLIvedToken(account.access_token);
+          await prisma.user.update({
             where: {
-              instagramId: user.id,
+              id: dbUser.id,
+            },
+            data: {
+              accessToken: longLivedToken,
+              refreshToken: account.refresh_token,
             },
           });
-          if (!dbUser && account.access_token) {
-            const longLivedToken = await getLongLIvedToken(account.access_token);
-            const {instaUserId, username, name} = await getInstagramBusinessAccount(account.access_token);
-            await prisma.user.create({
-              data: {
-                instagramId: user.id,
-                name: name,
-                email: user.email,
-                accessToken: longLivedToken,
-                refreshToken: account.refresh_token,
-                instaUserId: instaUserId,
-                instagramUsername:username
-              },
-            });
-          }
-          return true;
         }
-        return false;
-      },
-      async jwt({ token, account, user }) {
-        // Initial sign in
-        if (account && user) {
-          return {
-            accessToken: account.access_token,
-            refreshToken: account.refresh_token,
-            id: account.providerAccountId,
-            instagramId: user.id,
-            name: user.name,
-            email: user.email,
-            ...token
-          };
+        // create new user if not found
+        if (!dbUser && account.access_token) {
+          const longLivedToken = await getLongLIvedToken(account.access_token);
+          const { instaUserId, username, name } =
+            await getInstagramBusinessAccount(account.access_token);
+          await prisma.user.create({
+            data: {
+              instagramId: user.id,
+              name: name,
+              email: user.email,
+              accessToken: longLivedToken,
+              refreshToken: account.refresh_token,
+              instaUserId: instaUserId,
+              instagramUsername: username,
+            },
+          });
         }
-        
-        // Return previous token if the current token has not expired
-        return token;
-      },
-      async session({ session, token }) {
-        // Send properties to the client
-        if (token) {
-          session.accessToken = token.accessToken as string;
-          session.refreshToken = token.refreshToken as string;
-          if (session.user) {
-            session.user.id = token.id as string;
-            session.user.instagramId = token.instagramId as string;
-            session.user.name = token.name as string;
-            session.user.email = token.email as string;
-          }
-        }
-        return session;
-      },
+        return true;
+      }
+      return false;
     },
-  };
+    async jwt({ token, account, user }) {
+      // Initial sign in
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          id: account.providerAccountId,
+          instagramId: user.id,
+          name: user.name,
+          email: user.email,
+          ...token,
+        };
+      }
 
-  export default authOptions
+      // Return previous token if the current token has not expired
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client
+      if (token) {
+        session.accessToken = token.accessToken as string;
+        session.refreshToken = token.refreshToken as string;
+        if (session.user) {
+          session.user.id = token.id as string;
+          session.user.instagramId = token.instagramId as string;
+          session.user.name = token.name as string;
+          session.user.email = token.email as string;
+        }
+      }
+      return session;
+    },
+  },
+};
+
+export default authOptions;
